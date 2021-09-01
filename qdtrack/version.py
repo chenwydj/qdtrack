1ff4c2b953ac5072f005c0afaa728520160668a1
Wuyang Chen <chenwydj@gmail.com>
2021-09-01 01:20:34 +0000

    configs/bdd100k/qdtrack-frcnn_r50_fpn_12e_bdd100k.py

--- qdtrack/version.py
+++ qdtrack/version.py
@@ -1,5 +1,5 @@
 # GENERATED VERSION FILE
-# TIME: Mon Aug  9 18:56:15 2021
-__version__ = '0.1.0+5532425'
+# TIME: Mon Aug 23 21:55:17 2021
+__version__ = '0.1.0+b905d2a'
 short_version = '0.1.0'
 version_info = (0, 1, 0)
