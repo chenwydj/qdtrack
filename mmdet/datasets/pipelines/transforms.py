5686e3c7545bc991823ccbe4142351c401e1a68f
Wuyang Chen <chenwydj@gmail.com>
2021-11-30 22:56:39 -0600

    support merging complexity by t-1 frame and complexity on nature images

--- mmdet/datasets/pipelines/transforms.py
+++ mmdet/datasets/pipelines/transforms.py
@@ -699,8 +699,6 @@ class DropPatch:
         complexities = []
         assert len(image.shape) == 3
         img_h, img_w, _ = image.shape
-        from pdb import set_trace as bp
-        bp()
         grid_h, grid_w = int(np.ceil(img_h / self.grid_h)), int(np.ceil(img_w / self.grid_w)) # for avg_pool, number of grids, not height/width
         start_h = 0
         while start_h < img_h:
@@ -720,7 +718,6 @@ class DropPatch:
                 for x in range(grid_w):
                     complexities[y, x] = _complexities[max(0, y-1): min(grid_h-1, y+1)+1, max(0, x-1): min(grid_w-1, x+1)+1].mean()
             complexities = complexities.reshape(-1)
-        bp()
         locations_sorted = [x for _, x in sorted(zip(complexities, locations))]
         areas_sorted = [x for _, x in sorted(zip(complexities, areas))]
         return locations_sorted, areas_sorted, locations, areas, complexities
